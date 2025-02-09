54279f149f29ecb43b10444a2b53981a514b975b
SEVVALKARABAL <140542709+SEVVALKARABAL@users.noreply.github.com>
2025-02-09 14:55:21 +0300

    Add files via upload

---
+++ src/app/footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>
+          Made by{" "}
+          <a
+            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Coding Company
+          </a>
+          ™.
+        </p>
+      </footer>
+    </div>
+  );
+};
+export default Footer;
